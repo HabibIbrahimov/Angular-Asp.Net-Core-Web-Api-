c726db44dd0141d4e6e7f6aaa6ebd3a351c47e0d
HabibIbrahimov <habibyi@code.edu.az>
2022-06-17 16:31:12 +0400

    feat:DomainModels

---
+++ StudentManagement/DomainModels/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.DomainModels
+{
+    public class Gender
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
