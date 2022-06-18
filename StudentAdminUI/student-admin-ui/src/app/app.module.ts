0342cde7d7f63ba21ff5c23dde7e38d5f460558e
HabibIbrahimov <habibyi@code.edu.az>
2022-06-18 19:49:51 +0400

    feat:Filtering and Sorting

--- StudentAdminUI/student-admin-ui/src/app/app.module.ts
+++ StudentAdminUI/student-admin-ui/src/app/app.module.ts
@@ -42,6 +42,7 @@ import { MatTableModule } from '@angular/material/table';
 import { TopNavComponent } from './layout/top-nav/top-nav.component';
 import { StudentsComponent } from './students/students.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -89,7 +90,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatPaginatorModule,
     MatSortModule,
     MatTableModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   schemas :[
     CUSTOM_ELEMENTS_SCHEMA
--- StudentAdminUI/student-admin-ui/src/app/students/students.component.ts
+++ StudentAdminUI/student-admin-ui/src/app/students/students.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
@@ -14,6 +15,8 @@ export class StudentsComponent implements OnInit {
   displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
   dataSource:MatTableDataSource<Student> = new MatTableDataSource<Student>();
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  filterString = '';
   constructor(private studentService:StudentService) { }
 
   ngOnInit(): void {
@@ -23,12 +26,19 @@ export class StudentsComponent implements OnInit {
         this.students = success;
         this.dataSource = new MatTableDataSource<Student>(this.students);
         this.dataSource.paginator=this.paginator;
+        this.dataSource.sort=this.sort;
       },
       (err)=>
       {
 
       }
     )
+  }
+
+  filterStudents()
+  {
+    debugger;
+    this.dataSource.filter = this.filterString.trim().toLocaleLowerCase();
   }
 
 }
